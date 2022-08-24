b93a2e1f2473e3a360750da21227b79172562460
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2022-08-24 19:22:22 -0300

    add module to load proto file definitions

---
+++ src/proto/index.ts
@@ -0,0 +1,15 @@
+export * from "./__generated__/proto";
+export { MessageServiceHandlers } from "./__generated__/MessageService";
+
+import * as protoLoader from "@grpc/proto-loader";
+import { join } from "path";
+
+export const definitions = protoLoader.loadSync(
+  join(__dirname, "MessageService.proto"),
+  {
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  }
+);
