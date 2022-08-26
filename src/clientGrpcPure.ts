efa895d61c7e75de9480b090ac015fef45414691
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2022-08-26 16:11:11 -0300

    add gprc pure client

---
+++ src/clientGrpcPure.ts
@@ -0,0 +1,24 @@
+import * as grpc from "@grpc/grpc-js";
+import {
+  definitions,
+  MessageServiceDefinition,
+  MessageServiceClient
+} from "./proto";
+
+const grpcServerUrl = "127.0.0.1:8080";
+
+const messageProto = grpc.loadPackageDefinition(definitions).MessageService as grpc.ServiceClientConstructor;
+
+const client = new messageProto(grpcServerUrl, grpc.credentials.createInsecure()) as unknown as  MessageServiceClient
+
+client.SendMessage({
+  id: '0',
+  value: 1
+}, (err, response) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(response);
+  process.exit(0);
+})
