ea7847592e0e07c030c940883e3f3008c8bb3f23
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2022-08-26 16:23:53 -0300

    refactor server process

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
 import * as grpc from "@grpc/grpc-js";
-import { definitions, MessageServiceHandlers } from "./proto";
+import {
+  definitions,
+  MessageServiceHandlers,
+  IAcknowledgeMessage,
+  ISimpleMessage,
+} from "./proto";
 import { MessageServiceDefinition } from "./proto/__generated__/MessageService";
+
+const processMessage = (message: ISimpleMessage): IAcknowledgeMessage => {
+  const jsonMessage = JSON.stringify(message);
+  return {
+    message: `received: ${jsonMessage}`,
+    jsonsize: jsonMessage.length,
+  };
+};
 
 const handlers: MessageServiceHandlers = {
   SendMessage(call, cb) {
     console.log("called");
-    cb(null, {
-      message: `received data: ${JSON.stringify(call.request)}`,
-    });
+    cb(null, processMessage(call.request));
   },
 };
 
