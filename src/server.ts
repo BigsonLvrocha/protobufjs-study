51560a176ab6e38ebae20f3eb68f12ed0271c47e
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2022-08-26 15:59:23 -0300

    add logs to server

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,23 @@
 import * as grpc from '@grpc/grpc-js';
-import { definitions, MessageServiceHandlers } from './proto'
+import { definitions, MessageServiceHandlers,  } from './proto'
+import { MessageServiceDefinition } from './proto/__generated__/MessageService'
 
 const handlers: MessageServiceHandlers = {
   SendMessage(call, cb) {
+    console.log('called');
     cb(null, {
-      message: 'ok'
+      message: `received data: ${JSON.stringify(call.request)}`
     });
   }
 }
 
-
 const server = new grpc.Server();
-server.addService(definitions.MessageService as grpc.ServiceDefinition, handlers);
-server.bindAsync('localhost:8080', grpc.ServerCredentials.createInsecure(),  (error, port) => {
+server.addService(definitions.MessageService as any, handlers);
+server.bindAsync('127.0.0.1:8080', grpc.ServerCredentials.createInsecure(),  (error, port) => {
   if (error) {
     console.error(error);
     return;
   }
   console.log(`server running in port ${port}`);
+  server.start();
 })
