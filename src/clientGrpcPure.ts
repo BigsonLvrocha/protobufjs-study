b90821a259213b281beb86f7059a7a311a391944
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2022-08-26 16:12:10 -0300

    style

--- src/clientGrpcPure.ts
+++ src/clientGrpcPure.ts
@@ -2,23 +2,30 @@ import * as grpc from "@grpc/grpc-js";
 import {
   definitions,
   MessageServiceDefinition,
-  MessageServiceClient
+  MessageServiceClient,
 } from "./proto";
 
 const grpcServerUrl = "127.0.0.1:8080";
 
-const messageProto = grpc.loadPackageDefinition(definitions).MessageService as grpc.ServiceClientConstructor;
+const messageProto = grpc.loadPackageDefinition(definitions)
+  .MessageService as grpc.ServiceClientConstructor;
 
-const client = new messageProto(grpcServerUrl, grpc.credentials.createInsecure()) as unknown as  MessageServiceClient
+const client = new messageProto(
+  grpcServerUrl,
+  grpc.credentials.createInsecure()
+) as unknown as MessageServiceClient;
 
-client.SendMessage({
-  id: '0',
-  value: 1
-}, (err, response) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
+client.SendMessage(
+  {
+    id: "0",
+    value: 1,
+  },
+  (err, response) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(response);
+    process.exit(0);
   }
-  console.log(response);
-  process.exit(0);
-})
+);
