52bebc1e7f1ff4ec1a523acef512bf8f4cf11c38
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2022-09-01 14:34:07 -0300

    add print service

--- src/printMessages.ts
+++ src/printMessages.ts
@@ -1,13 +1,19 @@
 import { dirname, join } from "path";
 import { statSync } from "fs";
-import { SimpleMessage, MultipleMessages } from "./proto/__generated__/proto";
+import {
+  SimpleMessage,
+  MultipleMessages,
+  MessageService,
+  AcknowledgeMessage,
+} from "./proto/__generated__/proto";
 import * as multipleMessageJson from "../examples/MultipleMessage/json.json";
 import * as simpleMessageJson from "../examples/SimpleMessage/json.json";
 
 const simpleMessage = new SimpleMessage(simpleMessageJson);
 const multipleMessage = new MultipleMessages(multipleMessageJson);
-const simpleMessageEncoded = SimpleMessage.encode(simpleMessage).finish()
-const multipleMessageEncoded = MultipleMessages.encode(multipleMessage).finish()
+const simpleMessageEncoded = SimpleMessage.encode(simpleMessage).finish();
+const multipleMessageEncoded =
+  MultipleMessages.encode(multipleMessage).finish();
 
 const examplePath = join(dirname(__dirname), "examples");
 
@@ -19,13 +25,37 @@ const multipleJsonFileSize = statSync(
 );
 
 console.log("---------------- simple message -----------------------");
-console.log('json: ', JSON.stringify(simpleMessage))
-console.log('encoded: ', simpleMessageEncoded);
+console.log("json: ", JSON.stringify(simpleMessage));
+console.log("encoded: ", simpleMessageEncoded);
 console.log("json file size: ", simpleJsonFileSize.size);
-console.log("protobuf size: ", simpleMessageEncoded.length)
+console.log("protobuf size: ", simpleMessageEncoded.length);
 
 console.log("---------------- multiple message -----------------------");
-console.log('json: ', JSON.stringify(multipleMessage))
-console.log('encoded: ', multipleMessageEncoded);
+console.log("json: ", JSON.stringify(multipleMessage));
+console.log("encoded: ", multipleMessageEncoded);
 console.log("json file size: ", multipleJsonFileSize.size);
-console.log("protobuf size: ", multipleMessageEncoded.length)
+console.log("protobuf size: ", multipleMessageEncoded.length);
+
+console.log("---------------- messageService -------------------------");
+const service = new MessageService((method, data, cb) => {
+  const messageToAcknowledge = AcknowledgeMessage.create({
+    jsonsize: 2,
+    message: "ok",
+  });
+  const messageToAcknowledgeEncoded =
+    AcknowledgeMessage.encode(messageToAcknowledge).finish();
+  console.log("method called to service: ", method);
+  console.log("data sent to service: ", data);
+  console.log("data to be acknowledge: ", messageToAcknowledgeEncoded);
+  cb(null, messageToAcknowledgeEncoded);
+});
+
+console.log("sending message ", simpleMessage, " to service");
+service
+  .sendMessage(simpleMessage)
+  .then((response) => {
+    console.log("received response from server: ", response);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
